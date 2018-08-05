dd02599b63b6568a9a46a77c3952e23f2453c84a
Nabhoneel Majumdar <nabhoneel.95@gmail.com>
2018-08-05 21:01:49 +0530

    .

--- file-upload.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://www.djvu-pdf.com/"
-payload = {'q':'python'}
-r = requests.post(url, payload)
-with open("requests_results.html", "w") as f:
-    f.write(r.content)
